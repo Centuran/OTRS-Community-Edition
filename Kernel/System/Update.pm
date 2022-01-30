f7049a3a99a92a1a5794a9c582eadccef38ff7d6
Michał Wojciechowski <michal.wojciechowski@centuran.com>
2022-01-30 16:57:33 +0100

    Add basic system update modules

---
+++ Kernel/System/Update.pm
@@ -0,0 +1,226 @@
+# --
+# Copyright (C) 2021 Centuran Consulting, https://centuran.com/
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (GPL). If you
+# did not receive this file, see https://www.gnu.org/licenses/gpl-3.0.txt.
+# --
+
+package Kernel::System::Update;
+
+use strict;
+use warnings;
+
+use Archive::Tar;
+use Cwd;
+use Digest::MD5;
+use File::Basename;
+use File::Copy;
+use File::Path qw(make_path);
+
+use Kernel::System::Update::Database;
+
+our @ObjectDependencies = (
+    'Kernel::Config',
+    'Kernel::System::Cache',
+    'Kernel::System::DB',
+    'Kernel::System::FileTemp',
+    'Kernel::System::Log',
+    'Kernel::System::SysConfig',
+    'Kernel::System::Valid',
+);
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    my $Self = { };
+    bless( $Self, $Type );
+
+    return $Self;
+}
+
+sub CheckUpdate {
+    my ($Self, %Param) = @_;
+
+    # TODO: Check required parameters
+
+    # TODO: Check disk space
+
+    # TODO: Check HTTP server timeout
+
+    my $DistArchive = $Param{DistArchive};
+
+    # TODO: Check if $DistArchive exists and is readable
+}
+
+sub PerformUpdate {
+    my ($Self, %Param) = @_;
+
+    # TODO: Check parameters
+
+    my $DistArchive = $Param{DistArchive};
+
+    # TODO: Enable system maintenance mode
+
+    # TODO: Make backup
+
+    my $DistDir = $Param{DistFiles};# $Self->_ExtractDistArchive($DistArchive);
+
+    my $Home = $Kernel::OM->Get('Kernel::Config')->Get('Home');
+
+    my $CurrentDatabaseDir = $Home    . '/scripts/database';
+    my $NewDatabaseDir     = $DistDir . '/scripts/database';
+    
+    $Self->_UpdateDatabase($CurrentDatabaseDir, $NewDatabaseDir);
+
+    # TODO: Disable system maintenance mode
+}
+
+sub ExtractDistArchive {
+    my ($Self, %Param) = @_;
+
+    return $Self->_ExtractDistArchive($Param{Path});
+}
+
+sub FindModifiedFiles {
+    my ($Self, %Param) = @_;
+
+    my $CurrentPath = $Kernel::OM->Get('Kernel::Config')->Get('Home');
+
+    my $MD5 = Digest::MD5->new();
+
+    my $Cwd = cwd();
+    chdir($CurrentPath);
+
+    open(my $Archive, '<', $CurrentPath . '/ARCHIVE');
+
+    my @Files;
+
+    while (my $Line = <$Archive>) {
+        my ($Digest, $File) = split(/::/, $Line, 2);
+        chomp($File);
+
+        if (! -e "$CurrentPath/$File") {
+            # TODO: Error, file is supposed to exist
+            next;
+        }
+
+        open(my $In, '<', "$CurrentPath/$File"); # TODO: Handle open error
+
+        $MD5->addfile($In);
+
+        if ($MD5->hexdigest() ne $Digest) {
+            push @Files, $File;
+        }
+
+        close($In);
+
+        $MD5->reset();
+    }
+
+    close($Archive);
+
+    chdir($Cwd);
+
+    return @Files;
+}
+
+sub CopyFiles {
+    my ($Self, %Param) = @_;
+
+    my $CurrentPath = $Kernel::OM->Get('Kernel::Config')->Get('Home');
+    my $DistPath    = $Param{DistPath};
+
+    my $Cwd = cwd();
+    chdir($DistPath);
+
+    open(my $Archive, '<', $DistPath . '/ARCHIVE');
+
+    while (my $Line = <$Archive>) {
+        my (undef, $File) = split(/::/, $Line, 2);
+        chomp($File);
+
+        if (-e "$DistPath/$File") {
+            make_path(dirname("$CurrentPath/$File"), {
+                chmod => 0775
+            });
+            copy("$DistPath/$File", "$CurrentPath/$File")
+        }
+    }
+
+    close($Archive);
+
+    chdir($Cwd);
+}
+
+sub _CheckDistVersion {
+    my ($Self, $DistArchive) = @_;
+
+    my $Tar = Archive::Tar->new($DistArchive);
+    
+    my ($Dir) = $Tar->list_files();
+    my $Content = $Tar->get_content($Dir . 'RELEASE');
+    my ($Version) = ($Content =~ m/^ VERSION \s* = \s* ( \S+ ) $/mx);
+
+    return $Version;
+}
+
+# tar.gz or tar.bz2 supported
+sub _ExtractDistArchive {
+    my ($Self, $DistArchive) = @_;
+
+    # TODO: Check if $DistArchive exists and is readable
+
+    my $TempDir = $Kernel::OM->Get('Kernel::System::FileTemp')->TempDir();
+
+    my $Cwd = cwd();
+    chdir($TempDir);
+
+    my $Compression = COMPRESS_GZIP;
+    $Compression = COMPRESS_BZIP if $DistArchive =~ /\.bz2$/;
+
+    #Archive::Tar->extract_archive($DistArchive, $Compression);
+    system('tar ' . ($Compression == COMPRESS_GZIP ? 'z' : 'j') . 'xf ' . $DistArchive);
+    
+    chdir($Cwd);
+
+    my ($DistDir) = $Kernel::OM->Get('Kernel::System::Main')->DirectoryRead(
+        Directory => $TempDir,
+        Filter    => '*'
+    );
+
+    return $DistDir;
+}
+
+sub UpdateDatabase {
+    my ($Self, %Param) = @_;
+
+    # TODO: Check required params
+
+    my $MainObject = $Kernel::OM->Get('Kernel::System::Main');
+
+    my $CurrentPath = $Kernel::OM->Get('Kernel::Config')->Get('Home');
+    my $DistPath    = $Param{DistPath};
+
+    my $CurrentSchemaRef = $MainObject->FileRead(
+        Location => $CurrentPath . '/scripts/database/otrs-schema.xml'
+    );
+    my $DistSchemaRef = $MainObject->FileRead(
+        Location => $DistPath . '/scripts/database/otrs-schema.xml'
+    );
+    my $CurrentInitRef = $MainObject->FileRead(
+        Location => $CurrentPath . '/scripts/database/otrs-initial_insert.xml'
+    );
+    my $DistInitRef = $MainObject->FileRead(
+        Location => $DistPath . '/scripts/database/otrs-initial_insert.xml'
+    );
+
+    my $UpdateDBObject = Kernel::System::Update::Database->new;
+
+    $UpdateDBObject->UpdateSchema($$CurrentSchemaRef, $$DistSchemaRef);
+
+    $UpdateDBObject->UpdateData($$CurrentInitRef, $$DistInitRef);
+}
+
+
+1;
---
+++ Kernel/System/Update/Database.pm
@@ -0,0 +1,484 @@
+# --
+# Copyright (C) 2021 Centuran Consulting, https://centuran.com/
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (GPL). If you
+# did not receive this file, see https://www.gnu.org/licenses/gpl-3.0.txt.
+# --
+
+package Kernel::System::Update::Database;
+
+use strict;
+use warnings;
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    my $Self = {};
+    bless( $Self, $Type );
+
+    return $Self;
+}
+
+# Parses database schema as XML and returns an array of hashes representing
+# tables:
+# 
+# $Tables = [
+#     {
+#         Name    => 'user',
+#         Columns => [
+#             {
+#                 Name => 'login',
+#                 Type => 'VARCHAR'
+#             },
+#             ...
+#         ],
+#         Indices     => [ ... ],
+#         ForeignKeys => [ ... ],
+#         Unique      => [ ... ]
+#     },
+#     ...
+# ]
+sub ParseSchemaXML {
+    my ($Self, $XML) = @_;
+
+    my @Tables;
+
+    my (%Column, %ForeignKey, %Index, %IndexColumn, %Reference, %Table, %Unique,
+        %UniqueColumn);
+    my %Attrs = (
+        'Column' => [
+            qw( AutoIncrement Default Name PrimaryKey Required Size Type )
+        ],
+        'Reference' => [ qw( Foreign Local ) ]
+    );
+
+    my @Tags = $Kernel::OM->Get('Kernel::System::XML')->XMLParse(
+        String => $XML
+    );
+
+    for my $Tag (@Tags) {
+        # <Table>
+        if ($Tag->{Tag} eq 'Table') {
+            if ($Tag->{TagType} eq 'Start') {
+                %Table = (
+                    Name        => $Tag->{Name},
+                    Columns     => [],
+                    Indices     => [],
+                    ForeignKeys => [],
+                    Unique      => []
+                );
+            }
+            elsif ($Tag->{TagType} eq 'End') {
+                push @Tables, { %Table };
+            }
+        }
+        # <Column>
+        elsif ($Tag->{Tag} eq 'Column') {
+            if ($Tag->{TagType} eq 'Start') {
+                %Column = ();
+                for my $Attr (@{$Attrs{$Tag->{Tag}}}) {
+                    $Column{$Attr} = $Tag->{$Attr} if exists $Tag->{$Attr};
+                }
+            }
+            elsif ($Tag->{TagType} eq 'End') {
+                push @{$Table{Columns}}, { %Column };
+            }
+        }
+        # <Index>
+        elsif ($Tag->{Tag} eq 'Index') {
+            if ($Tag->{TagType} eq 'Start') {
+                %Index = (
+                    Name         => $Tag->{Name},
+                    IndexColumns => []
+                );
+            }
+            elsif ($Tag->{TagType} eq 'End') {
+                push @{$Table{Indices}}, { %Index };
+            }
+        }
+        # <IndexColumn>
+        elsif ($Tag->{Tag} eq 'IndexColumn') {
+            if ($Tag->{TagType} eq 'Start') {
+                %IndexColumn = (
+                    Name => $Tag->{Name}
+                );
+            }
+            elsif ($Tag->{TagType} eq 'End') {
+                push @{$Index{IndexColumns}}, { %IndexColumn };
+            }
+        }
+        # <Unique>
+        elsif ($Tag->{Tag} eq 'Unique') {
+            if ($Tag->{TagType} eq 'Start') {
+                %Unique = (
+                    Name          => $Tag->{Name},
+                    UniqueColumns => []
+                );
+            }
+            elsif ($Tag->{TagType} eq 'End') {
+                push @{$Table{Unique}}, { %Unique };
+            }
+        }
+        # <UniqueColumn>
+        elsif ($Tag->{Tag} eq 'UniqueColumn') {
+            if ($Tag->{TagType} eq 'Start') {
+                %UniqueColumn = (
+                    Name => $Tag->{Name}
+                );
+            }
+            elsif ($Tag->{TagType} eq 'End') {
+                push @{$Unique{UniqueColumns}}, { %UniqueColumn };
+            }
+        }
+        # <ForeignKey>
+        elsif ($Tag->{Tag} eq 'ForeignKey') {
+            if ($Tag->{TagType} eq 'Start') {
+                %ForeignKey = (
+                    ForeignTable => $Tag->{ForeignTable},
+                    References   => []
+                );
+            }
+            elsif ($Tag->{TagType} eq 'End') {
+                push @{$Table{ForeignKeys}}, { %ForeignKey };
+            }
+        }
+        # <Reference>
+        elsif ($Tag->{Tag} eq 'Reference') {
+            if ($Tag->{TagType} eq 'Start') {
+                %Reference = ();
+                for my $Attr (@{$Attrs{$Tag->{Tag}}}) {
+                    $Reference{$Attr} = $Tag->{$Attr} if exists $Tag->{$Attr};
+                }
+            }
+            elsif ($Tag->{TagType} eq 'End') {
+                push @{$ForeignKey{References}}, { %Reference };
+            }
+        }
+        # <database>
+        elsif ($Tag->{Tag} eq 'database') {
+            # No-op
+        }
+        else {
+            # TODO: Handle error properly
+            print STDERR "Unknown tag $Tag->{Tag}\n";
+            exit 1;
+        }
+    }
+
+    return \@Tables;
+}
+
+# Parses initial insert XML and returns an array of hashes, representing records
+# to be inserted into tables.
+#
+# $Tables = [
+#     {
+#         name => 'ticket',
+#         data => [
+#             {
+#                 Key     => 'title',
+#                 Content => 'Example Ticket'
+#             },
+#             ...
+#         ]
+#     },
+#     {
+#         ...    
+#     },
+#     ...
+# ]
+sub ParseInsertXML {
+    my ($Self, $XML) = @_;
+
+    my @Tables;
+
+    my ($Table, @Insert);
+
+    my @Tags = $Kernel::OM->Get('Kernel::System::XML')->XMLParse(
+        String => $XML
+    );
+
+    for my $Tag (@Tags) {
+        if ($Tag->{Tag} eq 'Insert') {
+            if ($Tag->{TagType} eq 'Start') {
+                ($Table) = grep { $_->{name} eq $Tag->{Table} } @Tables;
+
+                if (!defined $Table) {
+                    $Table = {
+                        name => $Tag->{Table},
+                        data => []
+                    };
+                    push @Tables, $Table;
+                }
+
+                @Insert = ();
+            }
+            elsif ($Tag->{TagType} eq 'End') {
+                push @{$Table->{data}}, [ @Insert ];
+            }
+        }
+        elsif ($Tag->{Tag} eq 'Data') {
+            if ($Tag->{TagType} eq 'Start') {
+                my $Data = {};
+                for my $Attr (qw( Key Translatable Type )) {
+                    $Data->{$Attr} = $Tag->{$Attr} if exists $Tag->{$Attr};
+                }
+                $Data->{Content} = $Tag->{Content};
+                push @Insert, $Data;
+            }
+        }
+        elsif ($Tag->{Tag} eq 'database') {
+            # No-op
+        }
+        else {
+            # TODO: Handle error properly
+            print STDERR "Unknown tag $Tag->{Tag}\n";
+            exit 1;
+        }
+    }
+
+    return \@Tables;
+}
+
+
+
+# Compares two hashes representing a database table before and after changes
+# (as returned by ParseSchemaXML) and returns a hash of differences:
+#
+# $TableDiff = {
+#     ColumnsAdded => [
+#         {
+#             Name => 'status',
+#             Type => 'INTEGER'
+#         },
+#         ...
+#     ],
+#     ColumnsChanged => [ ... ],
+#     ColumnsRemoved => [ ... ]
+# }
+sub TableDiff {
+    my ($Self, $TableBefore, $TableAfter) = @_;
+
+    my (@ColumnsAdded, @ColumnsChanged, @ColumnsRemoved);
+
+    my @ColumnsBefore = @{$TableBefore->{Columns}};
+    my @ColumnsAfter  = @{$TableAfter->{Columns}};
+
+    for my $Column (@ColumnsAfter) {
+        if (!grep { $_->{Name} eq $Column->{Name} } @ColumnsBefore) {
+            push @ColumnsAdded, $Column;
+        }
+    }
+
+    return if !(@ColumnsAdded || @ColumnsChanged || @ColumnsRemoved);
+    # return if !(@IndicesAdded ...)
+
+    return {
+        ColumnsAdded   => \@ColumnsAdded,
+        ColumnsChanged => \@ColumnsChanged,
+        ColumnsRemoved => \@ColumnsRemoved
+    };
+}
+
+# Generates XML document for database schema update based on results
+# of TableDiff.
+sub GenerateSchemaUpdateXML {
+    my ($Self, $Differences) = @_;
+
+    my $XML = '';
+
+    sub _Attr {
+        my ($Item, $Name) = @_;
+
+        if (exists $Item->{$Name}) {
+            my $Value = $Item->{$Name};
+
+            # Replace unsafe characters with entities
+            $Value =~ s/&/&amp;/sg;
+            $Value =~ s/</&lt;/sg;
+            $Value =~ s/>/&gt;/sg;
+            $Value =~ s/"/&quot;/sg;
+        
+            return "$Name=\"$Value\" ";
+        }
+        else {
+            return '';
+        }
+    }
+
+    for my $Table (@{$Differences->{TablesChanged}}) {
+        $XML .= sprintf("<TableAlter Name=\"%s\">\n", $Table->{Name});
+
+        for my $Column (@{$Table->{ColumnsAdded}}) {
+            $XML .= sprintf("<ColumnAdd Name=\"%s\" Type=\"%s\" %s/>\n",
+                $Column->{Name},
+                $Column->{Type},
+                _Attr($Column, 'AutoIncrement') .
+                _Attr($Column, 'Default') .
+                _Attr($Column, 'PrimaryKey') .
+                _Attr($Column, 'Required') .
+                _Attr($Column, 'Size')
+            );
+        }
+
+        $XML .= "</TableAlter>\n";
+    }
+
+    return $XML;
+}
+
+# Converts data for a single table, represented as an array (table) of arrays 
+# (record) of hashes (fields) into a string by concatenating all fields.
+sub SerializeInsertData {
+    my ($Self, $Data) = @_;
+
+    my $Serialized = '';
+
+    for my $Record (@$Data) {
+        for my $Field (@$Record) {
+            for my $Key (sort keys %$Field) {
+                $Serialized .= $Key . ' ' . ($Field->{$Key} // '') . ';';
+            }
+        }
+        $Serialized .= "\n";
+    }
+
+    return $Serialized;
+}
+
+sub UpdateSchema {
+    my ($Self, $CurrentSchema, $NewSchema) = @_;
+
+    my $CurrentTables = $Self->ParseSchemaXML($CurrentSchema);
+    my $NewTables     = $Self->ParseSchemaXML($NewSchema);
+
+    my (@TablesAdded, @TablesChanged);
+
+    for my $NewTable (@$NewTables) {
+        my ($CurrentTable) =
+            grep { $_->{Name} eq $NewTable->{Name} } @$CurrentTables;
+        
+        if (defined $CurrentTable) {
+            my $Diff = $Self->TableDiff($CurrentTable, $NewTable);
+
+            if (defined $Diff) {
+                push @TablesChanged, {
+                    Name => $NewTable->{Name},
+                    %$Diff
+                }
+            }
+        }
+        else {            
+            push @TablesAdded, $NewTable;
+        }
+    }
+
+    my $UpdateXML = $Self->GenerateSchemaUpdateXML({
+        TablesAdded   => \@TablesAdded,
+        TablesChanged => \@TablesChanged
+    });
+
+    if (length($UpdateXML) > 0) {
+        my $DBObject = $Kernel::OM->Get('Kernel::System::DB');
+
+        my @UpdateSQL = $DBObject->SQLProcessor(
+            Database => [
+                $Kernel::OM->Get('Kernel::System::XML')->XMLParse(
+                    String => $UpdateXML
+                )
+            ]
+        );
+
+        $DBObject->Do(SQL => $_) for @UpdateSQL;
+    }
+
+    return 1;
+}
+
+sub UpdateData {
+    my ($Self, $CurrentInit, $NewInit) = @_;
+
+    my $CurrentInserts = $Self->ParseInsertXML($CurrentInit);
+    my $NewInserts     = $Self->ParseInsertXML($NewInit);
+
+    my @CurrentTables = map { $_->{name} } @$CurrentInserts;
+    my @NewTables     = map { $_->{name} } @$NewInserts;
+    
+    my $DBObject = $Kernel::OM->Get('Kernel::System::DB');
+
+    Table:
+    for my $Table (@NewTables) {
+        my ($CurrentTable) = grep { $_ eq $Table } @CurrentTables;
+        
+        if (!defined $CurrentTable) {
+            # Table doesn't exist in current schema -- safe to insert all data
+
+            # TODO: Regenerate <Insert> tags from data
+        }
+        else {
+            # Serialize data
+            my $CurrentData =
+                (grep { $_->{name} eq $Table } @$CurrentInserts)[0]->{data};
+            my $CurrentDataSerialized =
+                $Self->SerializeInsertData($CurrentData);
+            my $NewData =
+                (grep { $_->{name} eq $Table } @$NewInserts)[0]->{data};
+            my $NewDataSerialized = $Self->SerializeInsertData($NewData);
+            
+            next Table if $CurrentDataSerialized eq $NewDataSerialized;
+
+            for my $Record (@$NewData) {
+                my ($IDField) = grep {
+                    exists $_->{Type} && $_->{Type} eq 'AutoIncrement'
+                } @$Record;
+
+                # Find the current record with the same ID
+                my ($CurrentRecord) = grep {
+                    grep { 
+                        exists $_->{Type} &&
+                            $_->{Type} eq 'AutoIncrement' &&
+                            $_->{Content} eq $IDField->{Content}
+                    } @$_
+                } @$CurrentData;
+
+                my @Conditions;
+
+                Field:
+                for my $Field (@$CurrentRecord) {
+                    if ($Field->{Key} =~ /_time$/ &&
+                        $Field->{Content} eq 'current_timestamp')
+                    {
+                        next Field;
+                    }
+
+                    my $Value = $Field->{Content};
+                    
+                    if (exists $Field->{Type} && $Field->{Type} eq 'Quote') {
+                        $Value = "'" . $DBObject->Quote($Value) . "'";
+                    }
+
+                    push @Conditions, $Field->{Key} . ' = ' . $Value;
+                }
+
+                my @Updates;
+
+                for my $Field (@$Record) {
+                    my $Value = $Field->{Content};
+
+                    if (exists $Field->{Type} && $Field->{Type} eq 'Quote') {
+                        $Value = "'" . $DBObject->Quote($Value) . "'";
+                    }
+
+                    push @Updates, $Field->{Key} . ' = ' . $Value;
+                }
+
+                my $UpdateQuery = sprintf("UPDATE $Table SET %s WHERE %s;",
+                    join(', ', @Updates), join(' AND ', @Conditions));
+            }
+        }
+    }
+
+}
+
+1;
