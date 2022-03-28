776751c62f9ed7020692db3f97e0fcc3b0958690
Michał Wojciechowski <michal.wojciechowski@centuran.com>
2022-03-28 03:27:00 +0200

    Modernize ticket search date dropdowns

--- Kernel/Modules/CustomerTicketSearch.pm
+++ Kernel/Modules/CustomerTicketSearch.pm
@@ -1846,6 +1846,7 @@ sub MaskForm {
         Translation => 0,
         Name        => 'TicketCreateTimePoint',
         SelectedID  => $Param{TicketCreateTimePoint},
+        Class       => 'Modernize',
     );
     $Param{TicketCreateTimePointStart} = $LayoutObject->BuildSelection(
         Data => {
@@ -1855,6 +1856,7 @@ sub MaskForm {
         Translation => 1,
         Name        => 'TicketCreateTimePointStart',
         SelectedID  => $Param{TicketCreateTimePointStart} || 'Last',
+        Class       => 'Modernize',
     );
     $Param{TicketCreateTimePointFormat} = $LayoutObject->BuildSelection(
         Data => {
@@ -1868,6 +1870,7 @@ sub MaskForm {
         Translation => 1,
         Name        => 'TicketCreateTimePointFormat',
         SelectedID  => $Param{TicketCreateTimePointFormat},
+        Class       => 'Modernize',
     );
     $Param{TicketCreateTimeStart} = $LayoutObject->BuildDateSelection(
         %Param,
