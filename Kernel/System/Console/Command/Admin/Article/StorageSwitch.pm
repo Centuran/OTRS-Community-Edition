72679cec306b4c44b403f6f0957a96fc3f54edaf
Michał Wojciechowski <michal.wojciechowski@centuran.com>
2022-05-08 02:47:49 +0200

    Add support for custom storage backends in StorageSwitch
    Based on changes in Znuny 6.0.35.

--- Kernel/System/Console/Command/Admin/Article/StorageSwitch.pm
+++ Kernel/System/Console/Command/Admin/Article/StorageSwitch.pm
@@ -14,11 +14,13 @@ use warnings;
 
 use parent qw(Kernel::System::Console::BaseCommand);
 
+use File::Basename qw(basename);
 use Time::HiRes qw(usleep);
 
 our @ObjectDependencies = (
     'Kernel::Config',
     'Kernel::System::DateTime',
+    'Kernel::System::Main',
     'Kernel::System::PID',
     'Kernel::System::Ticket',
 );
@@ -26,14 +28,23 @@ our @ObjectDependencies = (
 sub Configure {
     my ( $Self, %Param ) = @_;
 
+    my $Backends = join '|', sort $Self->_GetStorageBackends();
+
     $Self->Description('Migrate article files from one storage backend to another on the fly.');
     $Self->AddOption(
         Name        => 'target',
-        Description => "Specify the target backend to migrate to (ArticleStorageDB|ArticleStorageFS).",
+        Description => "Specify the target backend to migrate to ($Backends).",
         Required    => 1,
         HasValue    => 1,
-        ValueRegex  => qr/^(?:ArticleStorageDB|ArticleStorageFS)$/smx,
+        ValueRegex  => qr/^(?:$Backends)$/smx,
     );
+    $Self->AddOption(
+        Name        => 'source',
+        Description => "Specify the source backend to migrate from ($Backends).",
+        Required    => 0,
+        HasValue    => 1,
+        ValueRegex  => qr/^(?:$Backends)$/smx,
+    );    
     $Self->AddOption(
         Name        => 'tickets-closed-before-date',
         Description => "Only process tickets closed before given ISO date.",
@@ -149,10 +160,18 @@ sub Run {
     my $CountTotal = scalar @TicketIDs;
 
     my $Target        = $Self->GetOption('target');
-    my %Target2Source = (
+    my %TargetToSource = (
         ArticleStorageFS => 'ArticleStorageDB',
         ArticleStorageDB => 'ArticleStorageFS',
     );
+
+    my $Source = $TargetToSource{$Target} // $Self->GetOption('source');
+
+    if ( !$Source ) {
+        $Self->Print("<red>Source backend not specified.</red>\n");
+
+        return $Self->ExitCodeError();
+    }
 
     my $MicroSleep = $Self->GetOption('micro-sleep');
     my $Tolerant   = $Self->GetOption('tolerant');
@@ -166,7 +185,7 @@ sub Run {
 
         my $Success = $TicketObject->TicketArticleStorageSwitch(
             TicketID    => $TicketID,
-            Source      => $Target2Source{$Target},
+            Source      => $Source,
             Destination => $Target,
             UserID      => 1,
         );
@@ -186,6 +205,37 @@ sub PostRun {
     my ($Self) = @_;
 
     return $Kernel::OM->Get('Kernel::System::PID')->PIDDelete( Name => $Self->Name() );
+}
+
+sub _GetStorageBackends {
+    my ($Self) = @_;
+
+    my $BackendPath = 'Kernel/System/Ticket/Article/Backend/MIMEBase';
+    my @Directories = map {
+        $Kernel::OM->Get('Kernel::Config')->Get('Home') . $_ . $BackendPath
+    } qw( / /Custom/ );
+
+    my %Backends;
+
+    for my $Dir (@Directories) {
+        my @Files = $Kernel::OM->Get('Kernel::System::Main')->DirectoryRead(
+            Directory => $Dir,
+            Filter    => '*.pm',
+            Silent    => 1,
+        );
+
+        FILE:
+        for my $File (@Files) {
+            my $Basename = basename($File, '.pm');
+
+            # Ignore the "Base.pm" file
+            next FILE if $Basename eq 'Base';
+
+            $Backends{$Basename} = 1;
+        }
+    }
+
+    return keys %Backends;
 }
 
 1;
