3585c11fc10c0d197d1f7caa0d2bbcf53a0246b8
Michał Wojciechowski <michal.wojciechowski@centuran.com>
2022-05-10 23:48:09 +0200

    Fix translation in selection build method
    Based on changes in Znuny 6.0.37

--- Kernel/Output/HTML/Layout.pm
+++ Kernel/Output/HTML/Layout.pm
@@ -5473,13 +5473,6 @@ sub _BuildSelectionDataRefCreate {
             push @SortKeys, sort { lc $a cmp lc $b } ( values %List );
         }
 
-        # translate value
-        if ( $OptionRef->{Translation} ) {
-            for my $Row ( sort keys %{$DataLocal} ) {
-                $DataLocal->{$Row} = $Self->{LanguageObject}->Translate( $DataLocal->{$Row} );
-            }
-        }
-
         # sort hash (after the translation)
         if ( $OptionRef->{Sort} eq 'NumericKey' ) {
             @SortKeys = sort { $a <=> $b } ( keys %{$DataLocal} );
@@ -5837,6 +5830,13 @@ sub _BuildSelectionDataRefCreate {
                     }
                 }
             }
+        }
+    }
+
+    # translate value
+    if ( $OptionRef->{Translation} ) {
+        for my $Row ( @{$DataRef} ) {
+            $Row->{Value} = $Self->{LanguageObject}->Translate( $Row->{Value} );
         }
     }
 
